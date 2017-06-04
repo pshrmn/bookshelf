e89b89e6ff706af412f1b480f53280ee2d019043
Paul Sherman <paul.sherman.88@gmail.com>
2017-06-04 12:09:29 -0500

    Add genre descriptions, drop drama genre

--- src/components/pages/Genre.jsx
+++ src/components/pages/Genre.jsx
@@ -5,14 +5,17 @@ import Stats from 'components/Stats';
 import Showcase from 'components/Showcase';
 import Breadcrumbs from 'components/Breadcrumbs';
 
+import { genreMap } from 'constants/genres';
+
 function Genre(props) {
-  const { genre, books } = props;
+  const { genre, books, description } = props;
   return (
     <div className='genre-page'>
       <Breadcrumbs name='Genre'/>
       <h1>
         {genre} Books
       </h1>
+      <p>{description}</p>
       <Stats books={books} />
       <Showcase books={books} />
     </div>
@@ -24,6 +27,7 @@ export default connect(
     const { genre } = ownProps.params;
     return {
       genre: genre,
+      description: genreMap[genre].description,
       books: state.books.filter(book => book.genre === genre)
     }
   }
--- src/constants/genres.js
+++ src/constants/genres.js
@@ -1,44 +1,58 @@
 const genres = [
   {
     name: 'adventure',
-    className: 'adventure'
+    className: 'adventure',
+    description: 'Books in which the characters set off a journey that is filled with challenges.'
   },
   {
     name: 'dystopian',
-    className: 'dystopian'
+    className: 'dystopian',
+    description: 'Books in which the protagonists work against an oppressive group in power.'
   },
   {
     name: 'fantasy',
-    className: 'fantasy'
+    className: 'fantasy',
+    description: 'Books in which the author makes up a universe with things that cannot/do not' +
+      ' exist in our own (e.g. magic, mythical creatures).'
   },
   {
     name: 'historical',
-    className: 'historical'
+    className: 'historical',
+    description: 'Books that explore history. This includes alternative history books that' +
+      ' speculate about what might have happened if some historical events did not occur.'
   },
   {
     name: 'mystery',
-    className: 'mystery'
+    className: 'mystery',
+    description: 'Books in which the protagonists have to solve a problem with limited' +
+      ' help/evidence (often a crime).'
   },
   {
     name: 'non-fiction',
-    className: 'non-fiction'
+    className: 'non-fiction',
+    description: 'Books about real people, places, and things.'
   },
   {
     name: 'fiction',
-    className: 'fiction'
+    className: 'fiction',
+    description: 'A general catch-all genre for works of fiction that do not fit into any of' +
+      ' the other genres.'
   },
   {
     name: 'sci-fi',
-    className: 'sci-fi'
+    className: 'sci-fi',
+    description: 'A form of fantasy in which there is a scientific explanation for why things' +
+      ' work/are possible (no magic). These often take place in the future and in space or on a' +
+      ' different planet, but that is neither a requirement nor an indicator that a book is science' +
+      ' fiction.'
   },
   {
     name: 'thriller',
-    className: 'thriller'
-  },
-  {
-    name: 'drama',
-    className: 'drama'
-  },
+    className: 'thriller',
+    description: 'Books that are a type of mystery, but often have a fast pace and leave the' +
+      ' characters in perilous situations that leave the reader wanting to read more so that' +
+      ' they can learn what happens next.'
+  }
 ];
 
 export default genres;
