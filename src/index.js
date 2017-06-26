852b5d7ddfe6f7c4301b0ad8ab2319b40833c61f
Paul Sherman <paul.sherman.88@gmail.com>
2017-06-26 12:58:13 -0500

    Update to hickory

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { createHashHistory } from 'history';
+import { Hash } from 'hickory';
 import createConfig from 'curi';
 import { Navigator } from 'curi-react';
 import createAncestors from 'curi-addon-ancestors';
@@ -14,39 +14,40 @@ import bookLoader from './bookLoader';
 
 import 'sass/index.scss';
 
-const history = createHashHistory();
+const history = Hash();
 const config = createConfig(history, routes, {
   addons: [createAncestors]
 });
 
-const init = initialState => {
+const init = values => {
+
+  const [ initialState ] = values;
   const reducer = combineReducers(Object.assign({}, reducers));
   const store = createStore(
     reducer,
     initialState
   );
 
-  config.ready().then(() => {
-    ReactDOM.render((
-      <Provider store={store}>
-        <Navigator config={config}>
-          {renderFunction}
-        </Navigator>
-      </Provider>
-    ), document.querySelector('main'));
-  });
+  ReactDOM.render((
+    <Provider store={store}>
+      <Navigator config={config}>
+        {renderFunction}
+      </Navigator>
+    </Provider>
+  ), document.querySelector('main'));
 }
 
-bookLoader('data/books.json')
+const booksPromise = bookLoader('data/books.json')
   .then(resp => {
     if ( typeof resp === 'string' ) {
       resp = JSON.parse(resp);
     }
-
-    init({
+    return Promise.resolve({
       books: resp.books
     });
   })
   .catch(err => {
     console.error(`Failed to load books: ${err}`);
   });
+
+Promise.all([ booksPromise, config.ready() ]).then(init);
