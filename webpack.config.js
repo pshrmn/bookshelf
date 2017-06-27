5c889950e0c34259e2b38032d64429f28ea8793d
Paul Sherman <paul.sherman.88@gmail.com>
2017-06-27 11:51:42 -0500

    Drop HMR/dev-server from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const building = process.env.npm_lifecycle_event === 'build';
+
 const config = {
   context: path.join(__dirname, 'src'),
   entry: {
@@ -38,12 +40,20 @@ const config = {
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 2 } },
-          'postcss-loader',
-          'sass-loader'
-        ]
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                minimize: building
+              }
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
       }
     ]
   },
@@ -57,54 +67,7 @@ const config = {
   ]
 };
 
-switch(process.env.npm_lifecycle_event){
-case 'start':
-/*
-  config.entry.bundle = [
-    'webpack-dev-server/client?http://0.0.0.0:8000',
-    'webpack/hot/only-dev-server',
-    './index.js'
-  ]
-  // I am unsure of the best way to do this. Hard-coding
-  // will break if another loader is placed at the index
-  // that the jsx loader currently is. Replacing all of 
-  // the loaders seems cumbersome. This is ugly and having
-  // multiple loaders that match the file could cause issues
-  // (but there shouldn't be multiple loaders matching the
-  // test pattern).
-  config.module.loaders.forEach(l => {
-    if ( l.test.test('matches.jsx') ) {
-      l.loaders = ['react-hot-loader', 'babel-loader'];
-      delete l.loader;
-    }
-  });
-
-  config.plugins = config.plugins.concat([
-    new webpack.HotModuleReplacementPlugin()
-  ]);
-  
-  */
-  break;
-case 'build':
-  config.module.rules.forEach(l => {
-    if ( l.test.test('matches.scss') ) {
-      l.use = ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-              minimize: true
-            }
-          },
-          'postcss-loader',
-          'sass-loader'
-        ]
-      });
-    }
-  });
-  
+if (building) {
   config.plugins = config.plugins.concat([
     new webpack.DefinePlugin({
       'process.env': {
@@ -119,7 +82,6 @@ case 'build':
       }
     }),
   ]);
-  break;
 }
 
 module.exports = config;
