4a1ca58d227599676298dd848d04d28cb6b127d6
Paul Sherman <paul.sherman.88@gmail.com>
2017-11-06 20:38:04 -0600

    Only make book request once

--- src/store.js
+++ src/store.js
@@ -1,15 +1,17 @@
 import bookLoader from './bookLoader';
 import parseData from './parseData';
 
+const dataPromise = bookLoader('data/books.json')
+  .then(resp => {
+    if ( typeof resp === 'string' ) {
+      resp = JSON.parse(resp);
+    }
+    return Promise.resolve(parseData(resp.books));
+  })
+  .catch(err => {
+    console.error(`Failed to load books: ${err}`);
+  });
+
 export default function DataStore() {
-  return bookLoader('data/books.json')
-    .then(resp => {
-      if ( typeof resp === 'string' ) {
-        resp = JSON.parse(resp);
-      }
-      return Promise.resolve(parseData(resp.books));
-    })
-    .catch(err => {
-      console.error(`Failed to load books: ${err}`);
-    });
+  return dataPromise;
 }
